6c8ff480eeb386a312767f9758739cf22b60bf1d
atrain42 <maxheimer4211@gmail.com>
2022-08-22 14:14:47 -0400

    added form loader

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,61 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 
 import classes from "./Contact.module.css";
 import img from "../images/contact1.svg";
 import Footer from "../layout/Footer";
 
+import FormLoader from "../components/FormLoader/FormLoader";
+
 const Contact = () => {
+  const [formLoading, setFormLoading] = useState(false);
+
+  const handleSubmission = () => {
+    setFormLoading(true);
+  };
+
   return (
-    <div className={classes.contactBin}>
-      <h1 className="infoTitle">Let's get started</h1>
-      <section className={classes.contactSection}>
-        <img src={img} alt="contact" />
-        <form
-          action="https://formsubmit.co/pixel.invite.info@gmail.com"
-          method="POST"
-          className={classes.contactForm}
-        >
-          <input type="text" name="_honey" className={classes.honeypot} />
-          <input type="hidden" name="_captcha" value="false" />
-          <input
-            type="hidden"
-            name="_next"
-            value="https://www.pixelinvite.com/success"
-          />
-          <span>
+    <React.Fragment>
+      {formLoading && <FormLoader />}
+      <div className={classes.contactBin}>
+        <h1 className="infoTitle">Let's get started</h1>
+        <section className={classes.contactSection}>
+          <img src={img} alt="contact" />
+          <form
+            action="https://formsubmit.co/pixel.invite.info@gmail.com"
+            method="POST"
+            className={classes.contactForm}
+            onSubmit={handleSubmission}
+          >
+            <input type="text" name="_honey" className={classes.honeypot} />
+            <input type="hidden" name="_captcha" value="false" />
             <input
-              type="text"
-              name="First name"
-              placeholder="First Name"
-              required
+              type="hidden"
+              name="_next"
+              value="https://www.pixelinvite.com/success"
             />
-            <input
-              type="text"
-              name="Last Name"
-              placeholder="Last Name"
-              required
-            />
-          </span>
-          <span>
-            <input type="email" name="Email" placeholder="Email" required />
-            <select name="Payment method" required>
-              <option value="Apple Pay">Apple Pay</option>
-              <option value="Venmo">Venmo</option>
-              <option value="Cashapp">Cashapp</option>
-              <option value="PayPal">PayPal</option>
-            </select>
-          </span>
-          <textarea
-            cols="55"
-            rows="15"
-            placeholder="Message"
-            name="message"
-          ></textarea>
-          <button type="submit">Submit</button>
-        </form>
-      </section>
-      <Footer />
-    </div>
+            <span>
+              <input
+                type="text"
+                name="First name"
+                placeholder="First Name"
+                required
+              />
+              <input
+                type="text"
+                name="Last Name"
+                placeholder="Last Name"
+                required
+              />
+            </span>
+            <span>
+              <input type="email" name="Email" placeholder="Email" required />
+              <select name="Payment method" required>
+                <option value="Apple Pay">Apple Pay</option>
+                <option value="Venmo">Venmo</option>
+                <option value="Cashapp">Cashapp</option>
+                <option value="PayPal">PayPal</option>
+              </select>
+            </span>
+            <textarea
+              cols="55"
+              rows="15"
+              placeholder="Message"
+              name="message"
+            ></textarea>
+            <button type="submit">Submit</button>
+          </form>
+        </section>
+        <Footer />
+      </div>
+    </React.Fragment>
   );
 };
 
