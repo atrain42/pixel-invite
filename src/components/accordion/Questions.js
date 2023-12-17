f931098fc40113576f9547f509bc23abd6094871
atrain42 <maxheimer4211@gmail.com>
2023-12-17 18:25:22 -0500

    responsiveness updates

--- src/components/accordion/Questions.js
+++ src/components/accordion/Questions.js
@@ -7,11 +7,6 @@ import SectionHeader from "../sectionHeader/SectionHeader";
 
 const questions = [
   {
-    id: 1,
-    title: "Can anyone access my site?",
-    info: "Yes! Anyone who has the link to your website will be able to view the content. However, we offer a password protected version where we generate a random passwords to your site that protects it from unwanted guests.",
-  },
-  {
     id: 2,
     title: "How long does it take to build my site?",
     info: "We ask you to give us 2 - 3 days to complete your site. It is important to us that your site looks nice, so we take our time when completing it.",
@@ -24,12 +19,12 @@ const questions = [
   {
     id: 4,
     title: "Does my website stay up forever?",
-    info: "We take your website down a week after your baby shower. This prevents the information about your baby shower from living on the internet longer then it needs to. However, if you don't want us to take your site down, we won't.",
+    info: "We take your website down a week after your baby shower. This prevents the information about your baby shower from living on the internet longer then it needs to.",
   },
   {
     id: 5,
     title: "How does guest RSVP work?",
-    info: "When one of your guests fills out the contact form, the information on the form is directed to your email. This allows you to easily see who is planning on attending. Please inform us of the email you want to use.",
+    info: "When one of your guests fills out the contact form, the information on the form is sent directly to your email. This allows you to easily see who is planning on attending. Please inform us of the email you want to use.",
   },
 ];
 
--- src/components/accordion/SingleQuestion.js
+++ src/components/accordion/SingleQuestion.js
@@ -6,42 +6,56 @@ import classes from "./Question.module.css";
 
 const Question = ({ title, info }) => {
   const [text, setText] = useState("");
+  const [show, setShow] = useState(false)
 
   const showTextHandler = () => {
     setText(() => {
       return <p>{info}</p>;
     });
+    setShow(true)
   };
 
   const hideTextHandler = () => {
     setText("");
+    setShow(false)
   };
+
+  const toggleTextHandler = () => {
+    // Toggle between showTextHandler and hideTextHandler based on the current state
+    if (show) {
+      hideTextHandler()
+    } else {
+      showTextHandler()
+    }
+  }
+
+  const style = { fontSize: '5rem' }
 
   const buttonType = () => {
     if (text === "") {
       return (
         <button className={classes.btn} onClick={showTextHandler}>
-          <AiOutlinePlus />
+          <AiOutlinePlus style={style} />
         </button>
-      );
+      )
     } else {
       return (
         <button className={classes.btn} onClick={hideTextHandler}>
-          <AiOutlineMinus />
+          <AiOutlineMinus style={style} />
         </button>
-      );
+      )
     }
   };
 
   return (
-    <article className={classes.question}>
+    <article onClick={toggleTextHandler} className={classes.question}>
       <header>
         <h4>{title}</h4>
         {buttonType()}
       </header>
       <p>{text}</p>
     </article>
-  );
+  )
 };
 
 export default Question;
